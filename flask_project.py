330c330f8874d8368486ac59dd15846ced134b7c
maratkanov-a <a.s.maratkanov@gmail.com>
2016-01-28 00:11:06 +0300

    added func for handling GET

--- flask_project.py
+++ flask_project.py
@@ -2,16 +2,29 @@ from flask import Flask
 
 app = Flask(__name__)
 
+data = {
+    'hello': 'Hello World!',
+    'name': 'My name is Flask Server',
+}
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
 
+@app.route('/dictionary/<key>', methods=['GET'])
+def dict(key):
+    try:
+        result = data[key]
+    except KeyError:
+        # TODO: change to abort
+        result = '404'
+    return result
 
-@app.route('/dictionary')
-def dict():
-    return 'Hello World!'
+
+@app.route('/dictionary', methods=['POST'])
+def analog_dict():
+    return 'POST'
 
 
 if __name__ == '__main__':
