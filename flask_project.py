9bfb8f86a0309239a85c82b9693c4a9c53d8d7dd
maratkanov-a <a.s.maratkanov@gmail.com>
2016-01-28 01:21:33 +0300

    added rest funcs

--- flask_project.py
+++ flask_project.py
@@ -1,3 +1,4 @@
+import json
 from flask import Flask
 
 app = Flask(__name__)
@@ -13,18 +14,45 @@ def hello_world():
 
 
 @app.route('/dictionary/<key>', methods=['GET'])
-def dict(key):
+def seek_and_show(key):
     try:
         result = data[key]
     except KeyError:
-        # TODO: change to abort
         result = '404'
-    return result
+    return data
 
 
-@app.route('/dictionary', methods=['POST'])
-def analog_dict():
-    return 'POST'
+@app.route('/dictionary/', methods=['POST'])
+def add_element(request):
+    params = json.loads(request.data)
+    if not data.get(params.key):
+        data.update({params.key: params.value})
+        code = '200'
+    elif params.key and params.value:
+        code = 400
+    else:
+        code = 409
+    return code
+
+
+@app.route('/dictionary', methods=['PUT'])
+def change_element(request):
+    params = json.loads(request.data)
+    try:
+        data[request.key] = request.value
+        code = '200'
+    except KeyError:
+        code = '404'
+    return code
+
+
+@app.route('/dictionary/<key>', methods=['DELETE'])
+def delete_element(key):
+    try:
+        data.pop(key)
+    except KeyError:
+        pass
+    return '200'
 
 
 if __name__ == '__main__':
