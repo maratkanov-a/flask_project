042a6cd5e2825ae48f52510afa335369999ee395
maratkanov-a <a.s.maratkanov@gmail.com>
2016-01-28 18:12:36 +0300

    added get tests

--- tests.py
+++ tests.py
@@ -1,15 +1,39 @@
+import datetime
 import noseapp
+from noseapp.ext.requests import RequestsEx, make_config
+from requests import HTTPError
 
+endpoint = make_config()
+endpoint.configure(
+    base_url='http://127.0.0.1:5000/dictionary/',
+    key='127.0.0.1:5000'
+)
+endpoint.session_configure(
+    always_return_json=True,
+    raise_on_http_error=True
+)
+requests_ex = RequestsEx(endpoint)
 
 suite = noseapp.Suite('first_suite')
 
+api = requests_ex.get_endpoint_session('127.0.0.1:5000')
 
 @suite.register
 class TestCase(noseapp.TestCase):
+    
+    """
+        get request
+    """
+    def test_404(self):
+        self.assertEqual(HTTPError(404).message, api.get('123')['code'])
 
-    def test(self):
-        # do something
-        self.assertTrue(True)
+    def test_200(self):
+        # creepy
+        self.assertNotEqual(HTTPError(404).message, api.get('name')['name'])
+
+    # check time - must be equivalent to out time
+    def test_time(self):
+        self.assertEqual(api.get('name')['time'], datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
 
 
 @suite.register
